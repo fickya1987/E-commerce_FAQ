4a860474a3e45196d0f0ad2003b439194eb6b7da
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-05-29 12:33:51 +0700

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -36,9 +36,7 @@ def get_response(query):
     retriever = vectordb.as_retriever()
 
     # Prompt template
-    prompt_template = """Given the following context and a question, generate an answer based on this context only.
-    In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
-    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+    prompt_template = """ Merujuk pada konteks dan pertanyaan yang telah ada, berikan jawaban dari pertanyaan yang ditanyakan. Dalam menjawab pertanyaan, berikan jawaban sesuai dengan kolom bagian "response" tanpa banyak dirubah-rubah narasinya. Apabila tidak tahu, ucapkan saja saya tidak tahu atau silahkan tanya pertanyaan lain.
 
     CONTEXT: {context}
 
